647dfd52b680309893bb4e72c7564bc309068ef0
uiv10434 <uiv10434@vitescom.com>
2023-06-21 16:09:45 +0200

    Linq modify

--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -48,7 +48,11 @@ namespace DesktopContactsApp
             using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
             {
                 conn.CreateTable<Contact>();
-                contacts = conn.Table<Contact>().ToList();
+                contacts = (conn.Table<Contact>().ToList()).OrderBy(c => c.Name).ToList();
+
+                /*var variable = from c2 in contacts
+                               orderby  c2.Name
+                               select c2;*/
                 
             }
 
@@ -70,9 +74,15 @@ namespace DesktopContactsApp
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox searchTextBox = sender as TextBox;
-            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+            //var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
 
-            contactsListView.ItemsSource = filteredList;
+            //LINQ
+            var filteredList2 = (from c2 in contacts
+                                where c2.Name.ToLower().Contains(searchTextBox.Text.ToLower())
+                                orderby c2.Email
+                                select c2).ToList();
+
+            contactsListView.ItemsSource = filteredList2;
 
         }
     }
