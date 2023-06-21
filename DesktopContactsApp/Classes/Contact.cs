4b1f37dd2523167f93db8dd405112e396b354d55
uiv10434 <uiv10434@vitescom.com>
2023-06-21 11:37:01 +0200

    Defining ListView

--- DesktopContactsApp/Classes/Contact.cs
+++ DesktopContactsApp/Classes/Contact.cs
@@ -15,6 +15,10 @@ namespace DesktopContactsApp.Classes
         public string Email { get; set; }
         public string Phone { get; set; }
 
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Phone}";
+        }
 
     }
 }
